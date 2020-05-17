c1ba2043958f90cf7ea38c21dd3b258944731995
Sezer Ali <sezersinanoglu@gmail.com>
2020-05-18 02:10:52 +0300

    Entity sınıfları oluşturdum ve Layout Giydirmesi yaptım.

---
+++ mvcBlog/mvcBlog/Models/Model/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mvcBlog.Models.Model
+{
+    [Table("Admin")]
+    public class Admin
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required,StringLength(50,ErrorMessage ="50 Karakter Olmalıdır")]
+        public string Eposta { get; set; }
+       [Required,StringLength(50,ErrorMessage ="50 Karakter Olmalıdır")]
+        public string Sifre { get; set; }
+        public string Yetki { get; set; }
+    }
+}
---
+++ mvcBlog/mvcBlog/Models/Model/Hakkimizda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mvcBlog.Models.Model
+{
+    [Table("Hakkimizda")]
+    public class Hakkimizda
+    {
+        [Key]
+        public int ID { get; set; }
+        [DisplayName("Hakkımızda Açıklama")]
+        public string Aciklama { get; set; }
+    }
+}
